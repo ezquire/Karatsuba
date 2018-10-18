1097bbdab57ef2700b2fda66d9259102e0e108a5
ezquire <tyler.gearing@gmail.com>
2018-10-18 09:59:14 -0700

    added parsing function

--- suba.cpp
+++ suba.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
 // Function Prototypes
-vector<int> split(int);
+void split(int, vector<int>&);
 
 int main () {
 
@@ -31,18 +32,23 @@ int main () {
 		cin >> b;
 	}
 
-	parseA = split(a);
-	parseB = split(b);
+	split(a, parseA);
+	split(b, parseB);
 
 	while(choice < 3) {
 		cout << "\nEnter 1, 2, or 3\n1) Run Task 1\n2) Run Task 2\n3) Quit\n";
 		cin >> choice;
 		if(choice == 1) {
-			cout << "Running Task 1 with input: " << a << ", " << b;
+			cout << "Running Task 1 with input: " << a << ", " << b << endl;;
+			for(unsigned int i = 0; i < parseA.size(); ++i)
+				cout << parseA[i] << " ";
+			cout << endl;
+			for(unsigned int i = 0; i < parseB.size(); ++i)
+				cout << parseA[i] << " ";
 			cout << endl;
 		}
 		else if(choice == 2) {
-			cout << "Running Task 2 with input: " << a << ", " << b;
+			cout << "Running Task 2 with input: " << a << ", " << b << endl;;
 			cout << endl;
 		}
 	}
@@ -50,10 +56,22 @@ int main () {
 	return 0;
 }
 
-vector<int> split(int in) {
-	vector<int> out;
-	if(in % 2 == 0) {
-		
+void split(int in, vector<int>& parse) {
+	if(in == 1000) { // This handles the special case when A or B are 1000
+		parse.push_back(10);
+		parse.push_back(00);
 	}
-	return out;
+	int digits = ceil(log10((double) in));
+	switch(digits) {
+	case 0: // This handles the case when the input is 1
+		parse.push_back(in);
+		break;
+	case 1: // This handles all single digit numbers
+		parse.push_back(in);
+		break;
+	default: // This handles all 2 and 3 digit numbers
+		parse.push_back(in/10);
+		parse.push_back(in%10);
+		break;
+	}
 }
