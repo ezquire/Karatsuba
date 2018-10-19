b8ebfbf71c9b2c44aba3a42fed109fc7c49434e0
ezquire <tyler.gearing@gmail.com>
2018-10-19 10:59:50 -0700

    added skeleton for vector support

--- suba.cpp
+++ suba.cpp
@@ -3,23 +3,23 @@
 #include <vector>
 #include <math.h>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 // Function Prototypes
-//void split(int, vector<int>&, int&);
-long long product(int, int, int);
+vector<char> product(vector<char>, vector<char>, int);
 int numDigits(int);
 
 int main () {
 
-	int a = 0;
-	int b = 0;
+	string a;
+	string b;
 	int choice = 0;
 
 	cout << "Input an integer less than or equal to 1000 for A: ";
 	cin >> a;
-	while(a > 1000) {
+	while(stoi(a, NULL) > 1000) {
 		cout << "Error: input should be less than or equal to 1000\n";
 		cout << "Enter another value for A: ";
 		cin >> a;
@@ -27,7 +27,7 @@ int main () {
 
 	cout << "Input an integer less than or equal to 1000 for B: ";
 	cin >> b;
-	while(b > 1000) {
+	while(stoi(b, NULL) > 1000) {
 		cout << "Error: input should be less than or equal to 1000\n";
 		cout << "Enter another value for B: ";
 		cin >> b;
@@ -38,8 +38,8 @@ int main () {
 		cin >> choice;
 		if(choice == 1) {
 			cout << "Running Task 1 with input: " << a << ", " << b << endl;;
-			long long prod = product(a, b, 10);
-			cout << prod << endl;
+			//long long prod = product(a, b, 10);
+			cout << endl;
 		}
 		else if(choice == 2) {
 			cout << "Running Task 2 with input: " << a << ", " << b << endl;;
@@ -49,28 +49,6 @@ int main () {
 	
 	return 0;
 }
-
-/*void split(int in, vector<int>& parse, int& digits) {
-	if(in == 1000) { // This handles the special case when A or B are 1000
-		parse.push_back(10);
-		parse.push_back(00);
-	}
-	else {
-		digits = ceil(log10((double) in));
-		switch(digits) {
-		case 0: // This handles the case when the input is 1
-			parse.push_back(in);
-			break;
-		case 1: // This handles all single digit numbers
-			parse.push_back(in);
-			break;
-		default: // This handles all 2 and 3 digit numbers
-			parse.push_back(in/10);
-			parse.push_back(in%10);
-			break;
-		}
-	}
-	}*/
 
 int numDigits(int in) {
 	int digits = 0;
