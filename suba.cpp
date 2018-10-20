c60212ccc007d142d3bc770f8d36a145776307a9
ezquire <tyler.gearing@gmail.com>
2018-10-20 13:36:53 -0700

    new method to add strings

--- suba.cpp
+++ suba.cpp
@@ -1,4 +1,3 @@
-//commit shit
 #include <iostream>
 #include <iomanip>
 #include <math.h>
@@ -12,12 +11,13 @@ string product(string, string);
 string expo(string, string);
 string multiply_str(string, string);
 string add_str(string, string);
-string string_cat(string, string, string);
+//string string_cat(string, string, string);
+string string_add_large(string, string);
 
 int main () {
 
-	string a = "999";
-	string b = "999";
+	string a = "10000";
+	string b = "10000";
 	//int choice = 0;
 
 	cout << product(a, b) << endl;
@@ -71,6 +71,7 @@ string product(string x, string y) {
 	string c2;
 	string c0;
 	string c1;
+	long n = 0;
 	
 	if(x.length() < 2 && y.length() < 2) {
 		result = multiply_str(x, y);
@@ -88,15 +89,13 @@ string product(string x, string y) {
 		}
 
 		// Multiplier
-		int n = x.length();
+		n = x.length();
 		
 		// Creates strings of trailing 0s that will pad c2 and c1 
 		string pad2 = to_string((int)pow(10, n));
 		string pad1 = to_string((int)pow(10, n/2));
 		pad2 = pad2.substr(1);
 		pad1 = pad1.substr(1);
-
-		// W
 
 		// Splits the padded strings x and y into to halves
 		int half = x.length()/2;
@@ -109,7 +108,9 @@ string product(string x, string y) {
 		// Recursively call product to produce c2, c1, and c0
 		c2 = product(x1, y1);
 		c0 = product(x0, y0);
-		c1 = add_str( product(x1, y0), product(x0, y1));
+		
+		c1 = string_add_large(product(x1, y0), product(x0, y1));
+		//c1 = add_str(product(x1, y0), product(x0, y1));
 
 		// Pad c2 and c1 with 0s
 		c2 += pad2;
@@ -117,11 +118,14 @@ string product(string x, string y) {
 		
 		cout << "c0: " << c0 << " | c1: " << c1 << " | c2: " << c2 << endl;
 	}
-	result = string_cat(c0, c1, c2);
+	result += string_add_large(c0, c1);
+	//result += add_str(c0, c1);
+	result = string_add_large(result, c2);
+	//string result2 = add_str(result, c2);
 	return result;
 }
 
-string string_cat(string c0, string c1, string c2) {
+/*string string_cat(string c0, string c1, string c2) {
  	//if(c1 == "")
 	//	c1 = "0";
 
@@ -161,6 +165,51 @@ string string_cat(string c0, string c1, string c2) {
 			  
 	result = c2 + middle + right;
 	
+	return result;
+	}*/
+
+string string_add_large(string x, string y) {
+
+	// Swap strings in memory so the larger is second
+	if(x.length() > y.length())
+		swap(x, y);
+
+	string result = "";
+	int xLength = x.length();
+	int yLength = y.length();
+
+	// Need to reverse them so the rightmost digits are in the leftmost indices
+	reverse(x.begin(), x.end());
+	reverse(y.begin(), y.end());
+
+	int carry = 0;
+
+	for(int i = 0 ; i < xLength; ++i) {
+		int sum = ((x[i] - '0') + (y[i] - '0') + carry); // Convert and add
+		result.push_back(sum%10 + '0'); // Convert back to string and push
+		carry = sum/10; // Carry for the next step (grade school method)
+	}
+
+	// Add carries to the remaining digits of the larger number
+	for(int i = 0; i < yLength; ++i) {
+		int sum = ((y[i] - '0') + carry); // Convert and add
+		result.push_back(sum%10 + '0'); // Covert back to string and push
+	}
+
+	// If there is a remaining carry, convert to string and push
+	if(carry)
+		result.push_back(carry + '0');
+
+	// Remove leading 0s
+	//int i = 0;
+	//while(result[i] == '0')
+	//	++i;
+
+	//string ret = result.substr(i);
+
+	// Reverse result
+	reverse(result.begin(), result.end());
+
 	return result;
 }
 
